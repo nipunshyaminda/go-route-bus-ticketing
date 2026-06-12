8128c1b4bf4ab66cdc24fd16339e64e6ea061834
nipunshyaminda <nipunshyaminda4@gmail.com>
2026-06-12 14:54:07 +0530

    improved register page design

---
+++ frontend/js/script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("registerForm");
+
+if (form) {
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        const password = document.getElementById("password").value;
+        const confirmPassword = document.getElementById("confirmPassword").value;
+
+        if (password !== confirmPassword) {
+            alert("Passwords do not match");
+            return;
+        }
+
+        alert("Registration Successful");
+    });
+}
